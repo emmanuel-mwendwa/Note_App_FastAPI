56fdd13b6944c45ac9f4f1e9f65c19ad35ee4bb1
emmanuel-mwendwa <mwendwae054@gmail.com>
2024-01-14 19:39:25 +0300

    Route to get user

--- app/main.py
+++ app/main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI
 
 from . import models
 from .database import engine
-from .routers import notes
+from .routers import notes, users
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(notes.router)
+app.include_router(users.router)
 
 
 @app.get("/")
--- app/schemas.py
+++ app/schemas.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel
 class NoteOut(BaseModel):
     title: str
     content: str
+
+
+class UserOut(BaseModel):
+    email: str
